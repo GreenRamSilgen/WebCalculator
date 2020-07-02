752c10379b9f0128cb4bfa582d00c7dff362da19
GreenRamSilgen <ps3iswow@gmail.com>
2020-07-02 14:33:29 -0700

    Complete calculator with full funcionality. Mild/Bland color palette

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let display = document.querySelector("#mainDisplay");
 let values = [];
 let opers = [];
 let pressedEnter = false;
+let pressedDecimal = false;
 
 //Number checker
 const numbers = document.querySelectorAll(".numbers");
@@ -11,21 +12,26 @@ numbers.forEach(button => button.addEventListener('click', addToMain));
 const operator = document.querySelectorAll(".operation");
 operator.forEach(button => button.addEventListener('click', opButton));
 
+//Decimal checker
+const deci = document.querySelector("#dot");
+deci.addEventListener('click', deciPress);
+
+//Backspace checker
+const back = document.querySelector("#back");
+back.addEventListener('click', backSpace);
 //Enter checker
 const enter = document.querySelector("#enter");
-enter.addEventListener('click',enterPress);
+enter.addEventListener('click', enterPress);
 
 //Clear checker
 const checkClear = document.querySelector("#clear");
-checkClear.addEventListener('click',clear);
+checkClear.addEventListener('click', clear);
 
 //Display UPDATE on number press
 function addToMain(e) {
-    if(pressedEnter == false)
-    {
+    if (pressedEnter == false) {
         display.textContent += this.value;
-    }
-    else{
+    } else {
         clear();
         pressedEnter = false;
         display.textContent += this.value;
@@ -35,16 +41,14 @@ function addToMain(e) {
 //Update on Operation BUTTON PRESS
 function opButton(e) {
     console.log("Pressed ENTER?" + pressedEnter);
-    if(pressedEnter == false)
-    {
-    values.push(Number(display.textContent));
-    opers.push(this.value);
-    display.textContent = "";
-    console.log(values);
-    console.log(opers);
-    }
-    else
-    {
+    if (pressedEnter == false) {
+        values.push(Number(display.textContent));
+        opers.push(this.value);
+        pressedDecimal = false;
+        display.textContent = "";
+        console.log(values);
+        console.log(opers);
+    } else {
         pressedEnter = false;
         opers.push(this.value);
         display.textContent = "";
@@ -52,59 +56,77 @@ function opButton(e) {
 }
 
 
-
-function enterPress(e)
-{
+//PRESSED ENTER KEY
+function enterPress(e) {
     pressedEnter = true;
     console.log("PRessed Enter");
-    if(display.textContent != "")
-    {
+    if (display.textContent != "") {
         values.push(Number(display.textContent))
     }
-    
+
     console.log(values);
     console.log(opers);
     display.textContent = operate();
 }
 
-//Actual operation preform
-function operate()
-{
-    if(values.length <2) return;
-    if(values.length == 2) return calc(values[0],values[1],opers[0]);
+//PRESSED DECIMAL KEY
+function deciPress(e) {
+    if (pressedDecimal == false) {
+        if (pressedEnter == false) {
+            display.textContent += this.value;
+            pressedDecimal = true;
+        } else {
+            clear();
+            display.textContent += this.value;
+            pressedDecimal = true;
+            pressedEnter = false;
+        }
+    }
+}
 
-    let total = calc(values[0],values[1],opers[0]);
+//Actual operation preform
+function operate() {
+    if (values.length < 2) return;
+    pressedDecimal = false;
+    if (values.length == 2) return calc(values[0], values[1], opers[0]);
+
+    let total = calc(values[0], values[1], opers[0]);
     let numIdx = 2;
     let opIdx = 1;
-    while(numIdx != values.length)
-    {
-        total = calc(total,values[numIdx],opers[opIdx]);
+    while (numIdx != values.length) {
+        total = calc(total, values[numIdx], opers[opIdx]);
         opIdx++;
         numIdx++;
-    } 
+    }
     return total;
 }
 
 //perform operatin based on input
-function calc(val1, val2, op)
-{
-    if(op == "+") return val1 + val2;
-    if(op == "-") return val1 - val2;
-    if(op == "*") return val1 * val2;
-    if(op == "/")
-    { 
-        if(isNaN(val1/val2))
-        {
+function calc(val1, val2, op) {
+    if (op == "+") return val1 + val2;
+    if (op == "-") return val1 - val2;
+    if (op == "*") return val1 * val2;
+    if (op == "/") {
+        if (isNaN(val1 / val2)) {
             return 0;
         }
-        return val1/val2;
+        return val1 / val2;
     }
 }
 
-
+//Backspace function
+function backSpace(e) {
+    if(display.textContent != "")
+    {
+        if(pressedEnter == false)
+        {
+        if(display.textContent.charAt(display.textContent.length-1) == ".") pressedDecimal = false;
+        display.textContent = display.textContent.substring(0,display.textContent.length-1);
+        }
+    }
+}
 //Clear ALL
-function clear()
-{
+function clear() {
     values = [];
     opers = [];
     display.textContent = "";
